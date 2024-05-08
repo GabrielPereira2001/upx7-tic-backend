d59a678f605839376f7368b7dbc6c5130246ba67
GabrielPereira2001 <128058302+GabrielPereira2001@users.noreply.github.com>
2024-05-08 20:21:48 -0300

    Update GP ReportRepository.ts

--- src/repository/ReportRepository.ts
+++ src/repository/ReportRepository.ts
@@ -1,3 +1,22 @@
 import { ReportRepositorySequelize } from "./dependencies/ReportRepositorySequelize";
 
-export class ReportRepository extends ReportRepositorySequelize {};
+export class ReportRepository {
+    constructor(private db: any) {} 
+    public async findOpenReportsByCity(cityId: number): Promise<Report[]> {
+        try {
+            return this.db.Report.findAll({
+                where: {
+                    cityId: cityId,
+                    status: 'open'  
+                }
+            });
+        } catch (error) {
+            throw new Error('Error fetching reports: ' + error.message);
+        }
+    export class ReportService {
+    constructor(private reportRepository: ReportRepository) {}
+
+    public async getOpenReportsByCity(cityId: number): Promise<Report[]> {
+        return this.reportRepository.findOpenReportsByCity(cityId);
+    }
+}
